1d33f8b4fb3d8aec60e0267cbf2908718bc5748b
Ningi01 <86904400+Ningi01@users.noreply.github.com>
2021-07-04 21:06:09 +0100

    Update Index.php

--- Measurements Conversion/Index.php
+++ Measurements Conversion/Index.php
@@ -1,1 +1,26 @@
 
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Measurement Conversion</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+	<div id="main-content">
+	<h2>Php Mini Project By Theta Group.</h2>
+		<h1>Measurement Conversion</h1>
+	
+		<ul id="measurement-types">
+			<li><a href="length.php">Length and Distance</a></li>
+			<li><a href="area.php">Area</a></li>
+			<li><a href="volume.php">Volume and Capacity</a></li>
+			<li><a href="mass.php">Mass and Weight</a></li>
+			<li><a href="speed.php">Speed</a></li>
+			<li><a href="temperature.php">Temperature</a></li>
+		</ul>
+	</div>
+
+	<li><a href="theta.php"> List Of Theta Group Members</a></li>
+</body>
+</html>
