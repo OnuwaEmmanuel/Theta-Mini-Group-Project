b490d24d4cc74008fb70e1cbf8ee96b914b0cd96
Omo-to-sho <86947217+Omo-to-sho@users.noreply.github.com>
2021-07-11 12:33:45 +0000

    Update volume.php

--- Measurements Conversion/volume.php
+++ Measurements Conversion/volume.php
@@ -1,1 +1,54 @@
 
+<?php
+
+require_once('includes/functions.php');
+
+$from_value = '';
+$from_unit = '';
+$to_unit = '';
+$to_value = '';
+
+if(isset($_POST['submit'])) {
+  $from_value = $_POST['from_value'];
+  $from_unit = $_POST['from_unit'];
+  $to_unit = $_POST['to_unit'];
+  
+  $to_value = convert_volume($from_value, $from_unit, $to_unit);
+}
+
+$volume_options = array(
+  'cubic inches',
+  'cubic feet',
+  'Imperial gallons',
+  'Imperial quarts',
+  'Imperial pints',
+  'Imperial cups',
+  'Imperial ounces',
+  'Imperial tablespoons',
+  'Imperial teaspoons',
+  'US gallons',
+  'US quarts',
+  'US pints',
+  'US cups',
+  'US ounces',
+  'US tablespoons',
+  'US teaspoons',
+  'cubic centimeters',
+  'cubic meters',
+  'liters',
+  'milliliters'
+);
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Convert Volume</title>
+    <link href="styles.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+
+    <div id="main-content">
+
+      <h1>Convert Volume</h1>
