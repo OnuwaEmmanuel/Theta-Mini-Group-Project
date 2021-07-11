d75ed4be6e3ec44e74c1018df599d2b0a2b57faa
Abba300 <87151765+Abba300@users.noreply.github.com>
2021-07-11 11:42:53 +0000

    Update speed.php

--- Measurements Conversion/speed.php
+++ Measurements Conversion/speed.php
@@ -1,1 +1,35 @@
 
+<?php
+
+require_once('includes/functions.php');
+
+$from_value = '';
+$from_unit = '';
+$to_unit = '';
+$to_value = '';
+
+if(isset($_POST['submit'])) {
+  $from_value = $_POST['from_value'];
+  $from_unit = $_POST['from_unit'];
+  $to_unit = $_POST['to_unit'];
+  
+  $to_value = convert_speed($from_value, $from_unit, $to_unit);
+}
+
+$speed_options = array(
+  "feet per second",
+  "miles per hour",
+  "meters per second",
+  "kilometers per hour",
+  "knots"
+);
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Convert Speed</title>
+    <link href="styles.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
