8febbcb7088cb22b6ccd7efd0d622ec0743e202b
Krabiu442 <87104097+Krabiu442@users.noreply.github.com>
2021-07-12 16:05:21 +0100

    Update volume.php

--- Measurements Conversion/volume.php
+++ Measurements Conversion/volume.php
@@ -1,4 +1,3 @@
-
 <?php
 
 require_once('includes/functions.php');
@@ -52,3 +51,46 @@ $volume_options = array(
     <div id="main-content">
 
       <h1>Convert Volume</h1>
+
+<form action="" method="post">
+        
+        <div class="entry">
+          <label>From:</label>&nbsp;
+          <input type="text" name="from_value" value="<?php echo $from_value; ?>" />&nbsp;
+          <select name="from_unit">
+            <?php
+            foreach($volume_options as $unit) {
+              $opt = optionize($unit);
+              echo "<option value=\"{$opt}\"";
+              if($from_unit == $opt) { echo " selected"; }
+              echo ">{$unit}</option>";
+            }
+            ?>
+          </select>
+        </div>
+        
+        <div class="entry">
+          <label>To:</label>&nbsp;
+          <input type="text" name="to_value" value="<?php echo float_to_string($to_value); ?>" />&nbsp;
+          <select name="to_unit">
+            <?php
+            foreach($volume_options as $unit) {
+              $opt = optionize($unit);
+              echo "<option value=\"{$opt}\"";
+              if($to_unit == $opt) { echo " selected"; }
+              echo ">{$unit}</option>";
+            }
+            ?>
+          </select>
+          
+        </div>
+        
+        <input type="submit" name="submit" value="Submit" />
+      </form>
+  
+      <br />
+      <a href="index.php">Return to menu</a>
+      
+    </div>
+  </body>
+</html>
