285f16378b2a1e01e9d55867de9ab8b4895d1f09
Augustine Okeji <85004621+Amoto-austin@users.noreply.github.com>
2021-07-13 20:04:32 +0100

    Update speed.php

--- Measurements Conversion/speed.php
+++ Measurements Conversion/speed.php
@@ -32,3 +32,50 @@ $speed_options = array(
     <link href="styles.css" rel="stylesheet" type="text/css">
   </head>
   <body>
+
+    <div id="main-content">
+
+      <h1>Convert Speed</h1>
+  
+      <form action="" method="post">
+        
+        <div class="entry">
+          <label>From:</label>&nbsp;
+          <input type="text" name="from_value" value="<?php echo $from_value; ?>" />&nbsp;
+          <select name="from_unit">
+            <?php
+            foreach($speed_options as $unit) {
+              $opt = optionize($unit);
+              echo "<option value=\"{$opt}\"";
+              if($from_unit == $opt) { echo " selected"; }
+              echo ">{$unit}</option>";
+            }
+            ?>
+          </select>
+        </div>
+        
+        <div class="entry">
+          <label>To:</label>&nbsp;
+          <input type="text" name="to_value" value="<?php echo float_to_string($to_value); ?>" />&nbsp;
+          <select name="to_unit">
+            <?php
+            foreach($speed_options as $unit) {
+              $opt = optionize($unit);
+              echo "<option value=\"{$opt}\"";
+              if($to_unit == $opt) { echo " selected"; }
+              echo ">{$unit}</option>";
+            }
+            ?>
+          </select>
+          
+        </div>
+        
+        <input type="submit" name="submit" value="Submit" />
+      </form>
+  
+      <br />
+      <a href="index.php">Return to menu</a>
+      
+    </div>
+  </body>
+</html>
