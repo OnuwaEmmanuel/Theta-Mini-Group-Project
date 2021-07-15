0af4c264e980d41533516a1601cecc65e7a44323
ZbotAB <86911575+ZbotAB@users.noreply.github.com>
2021-07-15 17:22:34 +0100

    Update theta.php

--- Measurements Conversion/theta.php
+++ Measurements Conversion/theta.php
@@ -1,1 +1,49 @@
 
+<!DOCTYPE html>
+<html>
+
+<head>
+<title> Theta Group Members </title>
+
+<style> 
+h1
+{background-color: powderblue;
+    color: Maroon;
+}
+li{
+    font-family: cursive;
+}
+p {
+    font-family: cursive;
+}
+
+
+</style>
+
+</head>
+
+<body>
+ <h1> Students Behind The Success Of This Project </h1><br>
+
+<li> ONUWA Emmanuel Omirenyi        ...FCP/CSC/18/1062 </li><br>
+<li> UMAR Muhammad Kori             ...FCP/CCS/18/1011 </li><br>
+<li> UMAR Abdullahi                 ...FCP/CCS/18/1024</li><br>
+<li> OKORO Rita Amarachi            ...FCP/CSC/18/1044 </li><br>
+<li> YUSUF Shehu                    ...FCP/CCS/18/1031 </li><br>
+<li> USMAN Shafiu                   ...FCP/CCS/19/2006 </li><br>
+<li> MUHAMMAD Zainab                ...FCP/CIT/18/1018</li><br>
+<li> BAFFA Muhammad Saifullahi      ...FCP/CIT/18/1041</li><br>
+<li> SABO Sadiq Muhammad            ...FCP/CSC/18/1014  </li><br>
+<li> OTEGBEYE Omotosho Abdulsalam   ...FCP/CSC/18/1024</li><br>
+<li> NASIR Alamin Hanga             ...FCP/CSC/18/1028</li><br>
+<li> OKEJI Augustine Amoto          ...FCP/CSC/18/1041</li><br>
+<li> YUSUF Auwal Bala               ...FCP/CSC/18/1070</li><br>
+<li> SALIHU Abdullahi Umar          ...FCP/CSC/18/1073</li><br>
+<li> RABIU Kabiru                   ...FCP/CSC/19/2027</li><br>
+<li> SA'IDU Usama                   ...FCP/CSC/19/2031</li><br><br><br>
+
+<p> Thanks For You All Your Support - Onuwa Emmanuel Omirenyi (Group Leader)</p>
+
+</body>
+
+</html>
