504b1f01ffc022585ebfc21783970b6878aeb890
Abba300 <87151765+Abba300@users.noreply.github.com>
2021-07-21 10:24:00 +0000

    Create temperature.php

--- Measurements Conversion/temperature.php
+++ Measurements Conversion/temperature.php
@@ -1,1 +1,36 @@
+<?php
 
+require_once('includes/functions.php');
+
+$from_value = '';
+$from_unit = '';
+$to_unit = '';
+$to_value = '';
+
+if(isset($_POST['submit'])) {
+  $from_value = $_POST['from_value'];
+  $from_unit = $_POST['from_unit'];
+  $to_unit = $_POST['to_unit'];
+  
+  $to_value = convert_temperature($from_value, $from_unit, $to_unit);
+}
+
+$temp_options = array(
+  "Celsius",
+  "Fahrenheit",
+  "Kelvin"
+);
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Convert Temperature</title>
+    <link href="styles.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+
+    <div id="main-content">
+
+      <h1>Convert Temperature</h1>
